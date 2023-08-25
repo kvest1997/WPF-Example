0010df83066bb84fa15bc6289529498e07043fdc
Kirill <kvest1997@gmail.com>
2023-08-25 09:13:31 +0700

    Создание базового класса ViewModel

---
+++ WPF Example/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace WPF_Example.ViewModels.Base
+{
+    /// <summary>
+    /// Базовый класс ViewModel
+    /// </summary>
+    internal abstract class ViewModel : INotifyPropertyChanged, IDisposable
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Вызов события на изменения поля
+        /// </summary>
+        /// <param name="PropertyName"></param>
+        protected virtual void OnPropertyChanged([CallerMemberName]string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        /// <summary>
+        /// Метод на изменение поля
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="field">Поле\свойство</param>
+        /// <param name="value">значение</param>
+        /// <param name="PropertyName">название метода</param>
+        /// <returns></returns>
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
