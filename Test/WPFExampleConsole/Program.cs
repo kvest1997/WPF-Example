a944d1e95de749d2877ab9815d4b4827f909aa21
Kirill <kvest1997@gmail.com>
2023-08-28 15:41:52 +0700

    Запрос к удаленному серверу для получения сырых данных

--- Test/WPFExampleConsole/Program.cs
+++ Test/WPFExampleConsole/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace WPFExampleConsole
+﻿using System.Net;
+
+namespace WPFExampleConsole
 {
     internal class Program
     {
+        private const string data_url = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var client = new HttpClient();
+
+            var response = client.GetAsync(data_url).Result;
+            var csv_str = response.Content.ReadAsStringAsync().Result;
+
+            Console.ReadKey();
         }
     }
 }
