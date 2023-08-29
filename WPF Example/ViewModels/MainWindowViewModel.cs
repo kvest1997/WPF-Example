434a31bce2cd31794fbf00f8e430e43eca3de9d8
Kirill <kvest1997@gmail.com>
2023-08-29 16:54:57 +0700

    Строим график

--- WPF Example/ViewModels/MainWindowViewModel.cs
+++ WPF Example/ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,22 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
 using System.Windows.Input;
 using WPF_Example.Infrastructure.Commands;
+using WPF_Example.Models;
 using WPF_Example.ViewModels.Base;
 
 namespace WPF_Example.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
+        /// <summary>
+        /// Тестовый набор данных для визуализации графика
+        /// </summary>
+        private IEnumerable<DataPoint> _TestDataPoints;
+
+        public IEnumerable<DataPoint> TestDataPoints { get => _TestDataPoints; set => Set(ref _TestDataPoints, value); }
+
         #region Заголовок окна
         /// <summary>Заголовок окна</summary>
         private string _Title = "Анализ статистики";
@@ -63,6 +73,17 @@ namespace WPF_Example.ViewModels
                 CanCloseApplicationCommandExecute);
 
             #endregion
+
+
+            var data_points = new List<DataPoint>((int)(360 / 0.1));
+            for (var x = 0d; x <= 360; x+=0.1)
+            {
+                const double to_rad = Math.PI / 180;
+                var y = Math.Sin(x * to_rad);
+
+                data_points.Add(new DataPoint { XValue = x, YValue = y }); 
+            }
+            TestDataPoints = data_points;
         }
     }
 }
